454140d542032c8902d0c3697ac65d20885fb7e2
NazminJuli <nazminjuli@gmail.com>
2023-03-08 20:32:05 +0600

    clean commit

--- model.py
+++ model.py
@@ -1,26 +1,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
-#
-#
-# app = Flask(__name__)
-#
-# SECRET_KEY = 'secret key'
-#
-# # Configure Flask by providing the SQLALCHEMY_DATABASE_URI
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/MTT/PycharmProjects/One2Many/database.db'
-#
-# app.config['SECRET_KEY'] = SECRET_KEY
-#
-#
-# # Model declaration
-# # db = SQLAlchemy(app)
-# # api = Api(app)
-# api = Api()
-# db = SQLAlchemy()
-#
-# api.init_app(app)
-# db.init_app(app)
+
 db = SQLAlchemy()
 
 class Parents(db.Model):
@@ -36,18 +17,6 @@ class Parents(db.Model):
     state = db.Column(db.String(25), nullable=False)
     zip_code = db.Column(db.String(25), nullable=False)
 
-    # make a relationship with 'Child' model
-    #parent_child_relation = db.relationship("Child", backref=db.backref("parent"), cascade="all, delete-orphan")
-
-    #
-    # def __init__(self, firstname, lastname, street, city, state, zip_code):
-    #     self.firstname = firstname
-    #     self.lastname = lastname
-    #     self.street = street
-    #     self.city = city
-    #     self.state = state
-    #     self.zip_code = zip_code
-
 
 class Child(db.Model):
     # Child model
@@ -62,9 +31,6 @@ class Child(db.Model):
     parent_id = db.Column(db.Integer, ForeignKey('parent.id', ondelete="cascade"), nullable=False)
     pcrel = db.relationship('Parents',  backref = (db.backref("child", cascade="all, delete, delete-orphan")) )
 
-    # def __init__(self, child_firstname, child_lastname, parent_id):
-    #     self.child_firstname = child_firstname
-    #     self.child_lastname = child_lastname
-    #     self.parent_id = parent_id
+
 
 
